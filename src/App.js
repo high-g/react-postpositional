72e7056530a1a35a8b97651289651127e947795d
high-g <altema1217bbfumi@live.jp>
2022-11-13 00:27:35 +0900

    modify: やっつけ対応

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import { useEffect, useState } from 'react'
 import css from './App.module.css'
 
-const text = `私の名前はフロントエンジにゃーです。`
-
-// 辞書格納フォルダパス
-const DIC_URL = '/dict'
-
-// WordCloudの情報として抽出する品詞（助詞、助動詞などは意味がないので拾わない）
-const TARGET_POS = ['名詞', '動詞', '形容詞']
-
-// kuromoji.jsの解析結果の値で特に値がない場合は以下の文字が設定される
-const NO_CONTENT = '*'
+const text = `文字組の基礎`
 
 function App() {
   const [words, setWords] = useState([])
@@ -27,17 +18,6 @@ function App() {
       }
     })
 
-    // builder({ dicPath: DIC_URL }).build((err, tokenizer) => {
-    //   console.log('err', err)
-    //   if (err) {
-    //     console.log(err)
-    //     return
-    //   }
-
-    //   const tokens = tokenizer.tokenize(text)
-
-    //   console.log('tokens', tokens)
-    // })
     return () => {}
   }, [])
 
@@ -46,7 +26,7 @@ function App() {
       <p className={css.main}>
         {words.map((item, key) => {
           return (
-            <span key={key} className={item.pos === '助詞' && css.min}>
+            <span key={key} className={item.pos === '助詞' ? css.min : ''}>
               {item.surface_form}
             </span>
           )
